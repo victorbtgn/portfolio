2a139b498ad635643b2172de610d56d2ac08c48a
Victor Batigin <bixs85@gmail.com>
2020-11-19 23:52:12 +0200

    Updates

---
+++ precache-manifest.2b0371e355c731e2a4a37502a15b0b84.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7979c661e28488c0a4ba4055d9cf5581",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "41e827f82210c74538fe",
+    "url": "/portfolio/static/css/2.3d72dcb8.chunk.css"
+  },
+  {
+    "revision": "c10f7cc91237f0ee8c86",
+    "url": "/portfolio/static/css/main.e9670e39.chunk.css"
+  },
+  {
+    "revision": "41e827f82210c74538fe",
+    "url": "/portfolio/static/js/2.66da77dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.66da77dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c10f7cc91237f0ee8c86",
+    "url": "/portfolio/static/js/main.ef08728a.chunk.js"
+  },
+  {
+    "revision": "ccdaa0478081e22f8b3f",
+    "url": "/portfolio/static/js/runtime-main.6fee4835.js"
+  },
+  {
+    "revision": "919fc55297be5e12f9c8bf69024b0f35",
+    "url": "/portfolio/static/media/banner.919fc552.jpg"
+  },
+  {
+    "revision": "aaf13e8dff07a0cbb7b934c43ae2d6cb",
+    "url": "/portfolio/static/media/footer.aaf13e8d.jpg"
+  },
+  {
+    "revision": "9b719cbd34cbe46748ab4d1ba34075b0",
+    "url": "/portfolio/static/media/github.9b719cbd.svg"
+  },
+  {
+    "revision": "603d1c2928777847f84d046587da0e4a",
+    "url": "/portfolio/static/media/linkedin.603d1c29.svg"
+  },
+  {
+    "revision": "01e6c65ea7e7b2eda811909f43857b95",
+    "url": "/portfolio/static/media/movie-app.01e6c65e.png"
+  },
+  {
+    "revision": "f36d5c31e1ce79b542ef7ea86cc3ca22",
+    "url": "/portfolio/static/media/phonebook.f36d5c31.png"
+  },
+  {
+    "revision": "eac2ba9f73b3362a4cc0d3058c646401",
+    "url": "/portfolio/static/media/photo.eac2ba9f.JPG"
+  },
+  {
+    "revision": "c9d3bb26e1f6cacf59adf3070ab28749",
+    "url": "/portfolio/static/media/projects.c9d3bb26.jpg"
+  },
+  {
+    "revision": "66960d6b73c21373e6b477432c4b322d",
+    "url": "/portfolio/static/media/telegram.66960d6b.svg"
+  },
+  {
+    "revision": "17476405a7c5d41138a0514a7ed3f0f6",
+    "url": "/portfolio/static/media/wallpaper.17476405.jpg"
+  },
+  {
+    "revision": "ab4f5451d934486348ebf1b00d4e4f89",
+    "url": "/portfolio/static/media/weather-app.ab4f5451.png"
+  }
+]);
