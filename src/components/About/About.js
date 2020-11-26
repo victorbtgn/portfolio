0f01b2bab7fccb56a03c1b8462bcec2758dcd914
Victor Batigin <bixs85@gmail.com>
2020-11-26 16:00:48 +0200

    rework summary

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,7 +10,7 @@ export default function About() {
             <article className={styles.About}>
                 <h2 className={styles.title}>Hey, I"m Viktor</h2>
                 <div className={styles.img__container}></div>
-                <p className={styles.text}>A web developer from Bila Tserkva, Ukraine.</p>
+                <p className={styles.text}>I'm from Bila Tserkva, Ukraine. I'm looking for a position junior front-end developer. I want to find a family in which I can show all my skills and qualities as best I can and learn to become an even better developer. Ready to work well for best results. I strive to build working relationships based on trust and mutual respect. I adhere to the standards of conduct adopted by the company and the team.</p>
             </article>
         </ScrollableAnchor>
     )
